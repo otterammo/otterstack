47885cc6116d3f33e3aa6627345faad30ad179be
otterammo <dev-mattmoore@gmail.com>
2025-07-13 15:42:25 -0500

    feat: add monit

---
+++ core/monit/actions/unpause-downloads.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Pause jellyseerr container via Docker Engine API
+curl -s --unix-socket /var/run/docker.sock -X POST http://localhost/containers/jellyseerr/pause || true
+
+# Pause qbittorrent container via Docker Engine API
+curl -s --unix-socket /var/run/docker.sock -X POST http://localhost/containers/qbittorrent/unpause || true
